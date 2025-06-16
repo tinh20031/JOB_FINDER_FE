51efe4eaeae8929dc98bd8b23f7363f7f79e722d
nhathao <haonnqe170204@fpt.edu.vn>
2025-06-16 13:15:16 +0700

    Commit: Update linking Become Recruiter in component CallToAction (Button Start Recruiting Now in UI HomePage, Find Job, Company Listing)

--- components/job-listing-pages/components/CallToActions.jsx
+++ components/job-listing-pages/components/CallToActions.jsx
@@ -1,4 +1,26 @@
+'use client';
+
+import React, { useState } from "react";
+import { useRouter } from 'next/navigation';
+import BecomeRecruiterModal from "@/components/common/form/shared/BecomeRecruiterModal";
+
 const CallToActions = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const router = useRouter();
+
+  const handleOpenModal = () => {
+    const userId = localStorage.getItem('userId');
+    if (userId) {
+      setIsModalOpen(true);
+    } else {
+      router.push('/login');
+    }
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="call-to-action-four ">
       <h5>Recruiting?</h5>
@@ -6,13 +28,17 @@ const CallToActions = () => {
         Advertise your jobs to millions of monthly users and search 15.8 million
         CVs in our database.
       </p>
-      <a href="#" className="theme-btn btn-style-one bg-blue">
+      <button className="theme-btn btn-style-one bg-blue" onClick={handleOpenModal}>
         <span className="btn-title">Start Recruiting Now</span>
-      </a>
+      </button>
       <div
         className="image"
         style={{ backgroundImage: "url(/images/resource/ads-bg-4.png)" }}
       ></div>
+      <BecomeRecruiterModal
+        open={isModalOpen}
+        onCancel={handleCloseModal}
+      />
     </div>
   );
 };
